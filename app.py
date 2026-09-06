e478c4bd1ed57a134a2914a2299e82fb3df12006
anny234 <97annie79@gmail.com>
2026-09-06 11:19:43 +0300

    add basic logs

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from openpyxl.styles import Font, Alignment, PatternFill, Border, Side
 from openpyxl.utils.dataframe import dataframe_to_rows
 
 def process_file(content):
+    logging.info("Processing....")
     parsed_rows = []
     temp_items = []
     metadata = {"תעודה": "", "אסמכתא": "", "תאריך": ""}
@@ -56,6 +57,8 @@ def process_file(content):
     
     cols = ['תאריך', 'אסמכתא', 'תעודה', 'פריט', 'תיאור פריט', 'הזמנה', 'כמות', 'תאור י.מידה', 'מחיר ספק', 'ערך ספק']
     df = df[cols]
+
+    logging.info("Finished processing. Now saving file...")
 
     wb = openpyxl.Workbook()
     ws = wb.active
